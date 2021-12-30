c0e4029420a80400f7a0ff44f1cc16e59d4a2cc4
superfrost <otkycu@gmail.com>
2021-12-30 14:25:10 +0300

    add godotenv module

--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello world " + time.Now().String())
+	})
+
+	app.Get("/env", func(c *fiber.Ctx) error {
+		return c.SendString("Env var is: " + os.Getenv("MES"))
 	})
 
 	app.Get("/:name", func(c *fiber.Ctx) error {
@@ -21,5 +33,4 @@ func main() {
 	})
 
 	app.Listen(":" + os.Getenv("PORT"))
-
 }
