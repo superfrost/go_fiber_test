7700e8a0df9bbba9a8f99b70bd76dfe495e71a6e
superfrost <otkycu@gmail.com>
2021-12-30 13:47:05 +0300

    fix PORT env var for deployment

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,14 +12,14 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello world" + time.Now().String())
+		return c.SendString("Hello world " + time.Now().String())
 	})
 
 	app.Get("/:name", func(c *fiber.Ctx) error {
-		msg := fmt.Sprintf("Hello, %s!", c.Params("name"))
+		msg := fmt.Sprintf("Hello, %s! ", c.Params("name"))
 		return c.SendString(msg + fmt.Sprint(time.Now().Unix()))
 	})
 
-	app.Listen(":3000")
+	app.Listen(":" + os.Getenv("PORT"))
 
 }
